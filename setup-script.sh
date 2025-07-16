0ff2492a47fd42d0c690533c94eb9d3179e85df7
Wagner <wagneraraujo15@gmail.com>
2025-07-15 22:06:25 -0300

    add new softwares

--- setup-script.sh
+++ setup-script.sh
@@ -50,6 +50,8 @@ check_status
 show_message "Instalando dependências essenciais..."
 sudo apt install -y curl wget
 sudo apt install fd-find
+sudo apt install preload
+sudo apt install cpufrequtils
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
